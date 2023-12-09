5fbdd80a1dcef07f01cb14a045e0975259a3ed71
YutoShinohara <yuto.shino0128@gmail.com>
2023-12-09 10:17:15 +0900

    Initial Commit

---
+++ flask_uploader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flask_uploader
+//
+//  Created by 篠原優仁 on 2023/12/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flask_uploader/flask_uploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  flask_uploaderApp.swift
+//  flask_uploader
+//
+//  Created by 篠原優仁 on 2023/12/09.
+//
+
+import SwiftUI
+
+@main
+struct flask_uploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
